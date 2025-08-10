d7dd969ca7661d45b5faa340bb31e7fcd3a63464
Anu Sanodiya <anusanodiya81@gmail.com>
2025-08-10 23:52:41 +0530

    captainAuth done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,18 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import UserContext from './context/UserContext.jsx'
+import  CaptainContext  from './context/CaptainContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
- <React.StrictMode>
-   <BrowserRouter>
-     <UserContext>
-       <App />
-     </UserContext>
-   </BrowserRouter>
- </React.StrictMode>
+  <CaptainContext>
+    <UserContext>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserContext>
+  </CaptainContext>
+
 )
 
--- client/src/pages/UserLogin.jsx
+++ client/src/pages/UserLogin.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { UserDataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
-// import axios from 'axios'
+import axios from 'axios'
 
 const UserLogin = () => {
   const [ email, setEmail ] = useState('')
@@ -22,14 +22,14 @@ const UserLogin = () => {
       password: password
     }
 
-//     const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`, userData)
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`, userData)
 
-//     if (response.status === 200) {
-//       const data = response.data
-//       setUser(data.user)
-//       localStorage.setItem('token', data.token)
-//       navigate('/home')
-//     }
+    if (response.status === 200) {
+      const data = response.data
+      setUser(data.user)
+      localStorage.setItem('token', data.token)
+      navigate('/home')
+    }
 
 
     setEmail('')
